56f5db91ef08e582a15f870987eadce08a08d435
markusmajoros <134041168+markusmajoros@users.noreply.github.com>
2025-05-09 23:13:56 +0200

    test

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,40 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { getPages } from "~/sanityClient";
+import { PortableText } from "@portabletext/react";
+
+export async function loader() {
+  const pages = await getPages();
+  return {
+    date: new Date(),
+    pages,
+  };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  return (
+    <div>
+      {loaderData.pages.map((page) => (
+        <div>
+          <h2>{page.title}</h2>
+          <div>
+            <PortableText value={page.content} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export function headers() {
+  return {
+    "Cache-Control": "max-age: 3600,s-maxage: 7200",
+  };
+}
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
-}
-
-export default function Home() {
-  return <Welcome />;
 }
---
+++ app/sanityClient.ts
@@ -0,0 +1,18 @@
+// sanity.js
+import { createClient } from "@sanity/client";
+// Import using ESM URL imports in environments that supports it:
+// import {createClient} from 'https://esm.sh/@sanity/client'
+
+export const client = createClient({
+  projectId: "hqcxhqqf",
+  dataset: "production",
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion: "2025-02-06", // use current date (YYYY-MM-DD) to target the latest API version. Note: this should always be hard coded. Setting API version based on a dynamic value (e.g. new Date()) may break your application at a random point in the future.
+  // token: process.env.SANITY_SECRET_TOKEN // Needed for certain operations like updating content, accessing drafts or using draft perspectives
+});
+
+// uses GROQ to query content: https://www.sanity.io/docs/groq
+export async function getPages() {
+  const pages = await client.fetch('*[_type == "page"]');
+  return pages;
+}
