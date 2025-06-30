30cb38b3c482c83beafdbea5763b5440ebdf0c13
markusmajoros <134041168+markusmajoros@users.noreply.github.com>
2025-06-30 20:03:06 +0200

    feat: enhance admin route with styling and update Vite configuration to include Babel plugin

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,20 +1,25 @@
-"use no memo"
+"use no memo";
 
-import type {MetaFunction} from 'react-router'
-import { Studio } from 'sanity'
-import { Hydrated } from '~/components/hydrated'
+import type { LinksFunction, MetaFunction } from "react-router";
+import { Studio } from "sanity";
+import { Hydrated } from "~/components/hydrated";
+import studio from "../studio.css?url";
 
-import config from '../../sanity.config'
+import config from "../../sanity.config";
 
 export const meta: MetaFunction = () => [
-  {title: 'Sanity Studio'},
-  {name: 'robots', content: 'noindex'},
-]
+  { title: "Sanity Studio" },
+  { name: "robots", content: "noindex" },
+];
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: studio }];
+};
 
 export default function StudioPage() {
   return (
     <Hydrated>
       <Studio config={config} />
     </Hydrated>
-  )
+  );
 }
