4290d5d3921e1bf673f02873df718e551909e075
markusmajoros <134041168+markusmajoros@users.noreply.github.com>
2025-07-19 09:35:34 +0700

    fix: favicons

--- app/root.tsx
+++ app/root.tsx
@@ -33,37 +33,37 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
-        <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link
           rel="icon"
           type="image/png"
           sizes="192x192"
-          href="/favicon/android-chrome-192x192.png"
+          href="/android-chrome-192x192.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="512x512"
-          href="/favicon/android-chrome-512x512.png"
+          href="/android-chrome-512x512.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon/favicon-32x32.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon/favicon-16x16.png"
+          href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" /> <Links />
+        <link rel="manifest" href="/site.webmanifest" /> <Links />
       </head>
       <body>
         <div className="main-website-wrapper">
